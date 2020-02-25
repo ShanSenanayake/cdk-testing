12c4770ae0b86b0ac2db380f0f0bdb4ca548e4ee
Shan Senanayake <shan.senanayake@jayway.com>
2020-02-25 07:31:35 +0100

    remved unecessary user

--- lib/gleerups-dev-stack.ts
+++ lib/gleerups-dev-stack.ts
@@ -2,8 +2,6 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as kinesis from '@aws-cdk/aws-kinesis'
 import * as eventSources from '@aws-cdk/aws-lambda-event-sources';
-import * as iam from '@aws-cdk/aws-iam';
-import { User } from '@aws-cdk/aws-iam';
 
 export class GleerupsDevStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -16,10 +14,7 @@ export class GleerupsDevStack extends cdk.Stack {
       code: lambda.Code.fromAsset('lambda/subscriber')
     })
 
-  const user = new User(this, 'subscriber')
-
   kinesisStream.grantRead(lambdaFn)
-  kinesisStream.grantReadWrite(user);
 
   lambdaFn.addEventSource(new eventSources.KinesisEventSource(kinesisStream, {
     startingPosition: lambda.StartingPosition.LATEST,
